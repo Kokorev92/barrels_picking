9890e626c3782db5c397bb9f49dbe60adbca6b03
Kokorev92 <kokorev.yaroslav@gmail.com>
2023-03-15 14:56:53 +0300

    [wip]: класс спрайта машинки

---
+++ car.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self, x, y, speed, surf):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = surf
+        self.image = pygame.transform.scale(self.image, (100, 200))
+        self.rect = self.image.get_rect()
+        self.speed = speed
+
+    def update(self, *args):
+        self.rect.x = args[0]
+        self.rect.y = args[1]
+
